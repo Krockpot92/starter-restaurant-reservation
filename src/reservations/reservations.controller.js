0868be130de7b0c3fdf0c18a02d51409929d2c1b
Steven Lisboa <elstevo92@gmail.com>
2022-03-02 15:43:33 -0500

    us-01 progress

--- src/reservations/reservations.controller.js
+++ src/reservations/reservations.controller.js
@@ -1,21 +1,48 @@
 /**
  * List handler for reservation resources
  */
-const service = require("./reservations.service")
-const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
-
+const service = require("./reservations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function list(req, res) {
-  const data = await service.list()
-  res.json({ data });
+  const isQuery = req.query.date
+  console.log("Test", req.query.date, isQuery, req.params);
+  if (isQuery ) {
+    console.log("we in")
+    service.isQuery(isQuery)
+    .then((data) => res.send({ data }));
+  }
+
+  // const data = await service.list();
+  // res.json({ data });
 }
 
-async function create(req, res){
+function reservationExists(req, res, next) {
+  service
+    .read(req.params.reservationId)
+    .then((reservation) => {
+      if (reservation) {
+        res.locals.reservation = reservation;
+        return next();
+      }
+      next({ status: 404, message: `Reservation cannot be found.` });
+    })
+    .catch(next);
+}
+
+async function create(req, res) {
   const data = await service.create(req.body.data);
   res.status(201).json({ data });
 }
 
+async function destroy(req, res) {
+  const { reservation } = res.locals;
+  await service.destroy(reservation.reservation_id);
+  res.sendStatus(204);
+}
+
 module.exports = {
   list: asyncErrorBoundary(list),
-  create: asyncErrorBoundary(create)
+  create: asyncErrorBoundary(create),
+  delete: [reservationExists, asyncErrorBoundary(destroy)],
 };
--- src/reservations/reservations.service.js
+++ src/reservations/reservations.service.js
@@ -1,11 +1,26 @@
+const { select } = require("../db/connection");
 const knex = require("../db/connection");
 
 const tableName = "reservations";
 
 function list(){
     return knex(tableName)
-    .select("*")
+    .select("first_name")
 }
+
+function isQuery(date){
+    return knex(tableName)
+    .select("first_name",
+        "last_name",
+        "mobile_number",
+        "reservation_time",
+        "reservation_date")
+    .where({reservation_date : date})
+}
+
+function read(reservatinonId) {
+    return knex(tableName).select("*").where({ reservation_id: reservatinonId }).first();
+  }
 
 function create(newReservation){
     return knex(tableName)
@@ -14,7 +29,14 @@ function create(newReservation){
     .then((createdRecords)=> createdRecords[0])
 }
 
+function destroy(reservationId) {
+    return knex(tableName).where({ reservation_id: reservationId }).del();
+  }
+
 module.exports={
     list,
     create,
+    destroy,
+    read,
+    isQuery
 }
